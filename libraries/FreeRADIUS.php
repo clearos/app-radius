28f2fc273ae93eb72f360989be7ea82e9435d588
pcbaldwin <pbaldwin@clearfoundation.com>
2014-11-21 15:00:36 -0500

    Prepped for translation server

--- libraries/FreeRADIUS.php
+++ libraries/FreeRADIUS.php
@@ -323,7 +323,7 @@ class FreeRADIUS extends Daemon
         clearos_profile(__METHOD__, __LINE__);
 
         if (! preg_match('/^[a-zA-Z0-9_\-\.]+$/', $nickname))
-            return lang('radius_nickname_is_invalid');
+            return lang('radius_nickname_invalid');
     }
 
     /**
