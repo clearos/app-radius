9874b78f006abb706e47a9aa164698b3e9c7a06a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:40:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS प्रणाली अतिरिक्त प्रमाणीकरण यंत्रणा उपलब्ध आहे.';
+$lang['radius_app_name'] = 'दुरुपयोग';
+$lang['radius_client'] = 'क्लायंट';
+$lang['radius_clients'] = 'ग्राहक';
+$lang['radius_nickname'] = 'टोपणनाव';
+$lang['radius_nickname_invalid'] = 'टोपणनाव अवैध आहे.';
+$lang['radius_radius_server'] = 'दुरुपयोग';
