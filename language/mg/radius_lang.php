b7618cac1d621bb3e5268fa315e9a466f72accc7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:40:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Rayon manome fanamarinana fanampiny rafitra ho an&#39;ny rafitra.';
+$lang['radius_app_name'] = 'rayon Server';
+$lang['radius_client'] = 'Client';
+$lang['radius_clients'] = 'Clients';
+$lang['radius_nickname'] = 'Nickname';
+$lang['radius_nickname_invalid'] = 'Nickname dia tsy manan-kery.';
+$lang['radius_radius_server'] = 'rayon Server';
