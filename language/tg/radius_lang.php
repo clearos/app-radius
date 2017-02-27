6aa8d02f3d5962d94fd02a43c13b305e109a5af1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:44:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Радиусаш таъмин механизмҳои аутентификатсия иловагӣ барои системаи.';
+$lang['radius_app_name'] = 'радиусаш Server';
+$lang['radius_client'] = 'мизоҷ';
+$lang['radius_clients'] = 'мизоҷони';
+$lang['radius_nickname'] = 'лақаби';
+$lang['radius_nickname_invalid'] = 'Лақаби нодуруст аст.';
+$lang['radius_radius_server'] = 'радиусаш Server';
