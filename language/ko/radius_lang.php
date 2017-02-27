6ab86997624158000127070f2b343ba833ed6ff3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:39:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS 시스템 추가적인 인증 메커니즘을 제공한다.';
+$lang['radius_app_name'] = 'RADIUS 서버';
+$lang['radius_client'] = '고객';
+$lang['radius_clients'] = '클라이언트';
+$lang['radius_nickname'] = '별명';
+$lang['radius_nickname_invalid'] = '닉네임이 잘못되었습니다.';
+$lang['radius_radius_server'] = 'RADIUS 서버';
