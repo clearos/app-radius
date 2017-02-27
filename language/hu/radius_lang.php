5fe2c302efe96ff82df304b59ba5fd24e7d280f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:37:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS további hitelesítési mechanizmusok a rendszer.';
+$lang['radius_app_name'] = 'RADIUS Server';
+$lang['radius_client'] = 'Ügyfél';
+$lang['radius_clients'] = 'Az ügyfelek';
+$lang['radius_nickname'] = 'Becenév';
+$lang['radius_nickname_invalid'] = 'Becenév érvénytelen.';
+$lang['radius_radius_server'] = 'RADIUS Server';
