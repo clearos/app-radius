3f81f443ee2ba95d0db3f9309149d68987f3771f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:37:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'त्रिज्या प्रणाली के लिए अतिरिक्त प्रमाणीकरण तंत्र प्रदान करता है।';
+$lang['radius_app_name'] = 'त्रिज्या सर्वर';
+$lang['radius_client'] = 'ग्राहक';
+$lang['radius_clients'] = 'ग्राहकों';
+$lang['radius_nickname'] = 'उपनाम';
+$lang['radius_nickname_invalid'] = 'उपनाम अमान्य है।';
+$lang['radius_radius_server'] = 'त्रिज्या सर्वर';
