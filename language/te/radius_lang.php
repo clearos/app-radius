8d3c3e0f55f4e3b2232f1f03cfabf2f8ca20be6e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:44:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'వ్యాసార్థం వ్యవస్థ కోసం అదనపు ధృవీకరణ పద్దతులకు అందిస్తుంది.';
+$lang['radius_app_name'] = 'వ్యాసార్థం సర్వర్';
+$lang['radius_client'] = 'క్లయింట్';
+$lang['radius_clients'] = 'క్లయింట్లు';
+$lang['radius_nickname'] = 'మారుపేరు';
+$lang['radius_nickname_invalid'] = 'మారుపేరు చెల్లదు.';
+$lang['radius_radius_server'] = 'వ్యాసార్థం సర్వర్';
