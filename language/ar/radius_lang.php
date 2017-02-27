6277fdcf34af4842013fbfed6205ef63fae3b8b1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:33:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'يوفر RADIUS آليات مصادقة إضافية للنظام.';
+$lang['radius_app_name'] = 'نصف قطرها الخادم';
+$lang['radius_client'] = 'زبون';
+$lang['radius_clients'] = 'عملاء';
+$lang['radius_nickname'] = 'كنية';
+$lang['radius_nickname_invalid'] = 'اللقب هو غير صالح.';
+$lang['radius_radius_server'] = 'نصف قطرها الخادم';
