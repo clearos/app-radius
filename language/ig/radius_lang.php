cafae0062bb5d9bc3eb32ec410559b5381e21535
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:38:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Were n&#39;okirikiri RADIUS-enye ndị ọzọ Nyocha usoro maka usoro.';
+$lang['radius_app_name'] = 'were n&#39;okirikiri RADIUS Server';
+$lang['radius_client'] = 'Client';
+$lang['radius_clients'] = 'ahịa';
+$lang['radius_nickname'] = 'aha otutu';
+$lang['radius_nickname_invalid'] = 'Aha otutu bụ ghara ịdị irè.';
+$lang['radius_radius_server'] = 'were n&#39;okirikiri RADIUS Server';
