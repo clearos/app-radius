ef6f71260dbdd811507a15b2077fd7ef1ba1befd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:38:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Radíus afla frekari staðfesting aðferðir til kerfisins.';
+$lang['radius_app_name'] = 'radíus Server';
+$lang['radius_client'] = 'viðskiptavinur';
+$lang['radius_clients'] = 'viðskiptavinir';
+$lang['radius_nickname'] = 'gælunafn';
+$lang['radius_nickname_invalid'] = 'Gælunafn er ógilt.';
+$lang['radius_radius_server'] = 'radíus Server';
