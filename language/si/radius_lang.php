12ed0a60e2632cacfaf5dacae73cf72c397dea90
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:43:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'අරය පද්ධතිය සඳහා අතිරේක සත්යාපන යාන්ත්රණ සපයයි.';
+$lang['radius_app_name'] = 'අරය සේවාදායකය';
+$lang['radius_client'] = 'සේවාලාභියා';
+$lang['radius_clients'] = 'සේවාලාභීන්';
+$lang['radius_nickname'] = 'අපනාමය';
+$lang['radius_nickname_invalid'] = 'අපනාමය වලංගු නැත.';
+$lang['radius_radius_server'] = 'අරය සේවාදායකය';
