a7941eb35e55246757c9c7ee1efd70763574a442
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:41:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS ਸਿਸਟਮ ਲਈ ਵਾਧੂ ਪ੍ਰਮਾਣਿਕਤਾ ਢੰਗ ਦਿੰਦਾ ਹੈ.';
+$lang['radius_app_name'] = 'RADIUS ਸਰਵਰ';
+$lang['radius_client'] = 'ਕਲਾਇੰਟ';
+$lang['radius_clients'] = 'ਗ੍ਰਾਹਕ';
+$lang['radius_nickname'] = 'ਉਪਨਾਮ';
+$lang['radius_nickname_invalid'] = 'ਉਪਨਾਮ ਗਲਤ ਹੈ.';
+$lang['radius_radius_server'] = 'RADIUS ਸਰਵਰ';
