1d38f01c98b35ea5b6b31525f7a68bf51f3baa43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:45:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS надає додаткові механізми аутентифікації системи.';
+$lang['radius_app_name'] = 'RADIUS-сервер';
+$lang['radius_client'] = 'клієнт';
+$lang['radius_clients'] = 'клієнти';
+$lang['radius_nickname'] = 'кличка';
+$lang['radius_nickname_invalid'] = 'Нік є недійсним.';
+$lang['radius_radius_server'] = 'RADIUS-сервер';
