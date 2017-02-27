7f82df47d404a92102949103c85fdf32c09214a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:33:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Radius siguron mekanizma shtesë autentikimit për sistemin.';
+$lang['radius_app_name'] = 'Radius Server';
+$lang['radius_client'] = 'klient';
+$lang['radius_clients'] = 'klientët';
+$lang['radius_nickname'] = 'Nickname';
+$lang['radius_nickname_invalid'] = 'Pseudonimi është e pavlefshme.';
+$lang['radius_radius_server'] = 'Radius Server';
