7920b7776a0bd0837b174ac0c5a526f8283bfea1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:40:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS സിസ്റ്റം അധിക പ്രാമാണീകരണ സംവിധാനം നൽകുന്നു.';
+$lang['radius_app_name'] = 'RADIUS സെർവർ';
+$lang['radius_client'] = 'കക്ഷി';
+$lang['radius_clients'] = 'ഉപഭോക്താക്കള്ക്ക്';
+$lang['radius_nickname'] = 'വിളിപ്പേര്';
+$lang['radius_nickname_invalid'] = 'വിളിപ്പേര് അസാധുവാണ്.';
+$lang['radius_radius_server'] = 'RADIUS സെർവർ';
