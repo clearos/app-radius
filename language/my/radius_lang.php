b5a19ca0484616a6e5c45a84e1a3f324737fbdfd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:35:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS စနစ်များအတွက်အပိုဆောင်း authentication ကိုယန္တရားများပေးစွမ်းသည်။';
+$lang['radius_app_name'] = 'RADIUS Server ကို';
+$lang['radius_client'] = 'ဖောက်သည်';
+$lang['radius_clients'] = 'clients';
+$lang['radius_nickname'] = 'အမည်ပြောင်';
+$lang['radius_nickname_invalid'] = 'nickname မမှန်ကန်ဘူး။';
+$lang['radius_radius_server'] = 'RADIUS Server ကို';
