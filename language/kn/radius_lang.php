1d92e1b8d8a377777b7c705b6b2297fb77ce516a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:39:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'ತ್ರಿಜ್ಯ ವ್ಯವಸ್ಥೆಯ ಹೆಚ್ಚುವರಿ ದೃಢೀಕರಣ ತಂತ್ರಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['radius_app_name'] = 'ತ್ರಿಜ್ಯ ಸರ್ವರ್';
+$lang['radius_client'] = 'ಕ್ಲೈಂಟ್';
+$lang['radius_clients'] = 'ಗ್ರಾಹಕರು';
+$lang['radius_nickname'] = 'ಅಡ್ಡಹೆಸರು';
+$lang['radius_nickname_invalid'] = 'ಅಡ್ಡಹೆಸರು ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['radius_radius_server'] = 'ತ್ರಿಜ್ಯ ಸರ್ವರ್';
