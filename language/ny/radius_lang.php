2845bfef6697a648860075200fa34eae774dd985
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:35:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Utali wozungulira amapereka njira zina kutsimikizika kwa dongosolo.';
+$lang['radius_app_name'] = 'utali wozungulira Server';
+$lang['radius_client'] = 'kasitomala';
+$lang['radius_clients'] = 'makasitomala';
+$lang['radius_nickname'] = 'dzina';
+$lang['radius_nickname_invalid'] = 'Dzina ili udzakhala osazindikilika.';
+$lang['radius_radius_server'] = 'utali wozungulira Server';
