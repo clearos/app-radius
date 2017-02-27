c5b01e97df3055fc8266055a1529c0db4e38cfa7
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:38:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUSは、システムの追加の認証メカニズムを提供します。';
+$lang['radius_app_name'] = 'RADIUSサーバ';
+$lang['radius_client'] = 'クライアント';
+$lang['radius_clients'] = 'クライアント';
+$lang['radius_nickname'] = 'ニックネーム';
+$lang['radius_nickname_invalid'] = 'ニックネームは無効です。';
+$lang['radius_radius_server'] = 'RADIUSサーバ';
