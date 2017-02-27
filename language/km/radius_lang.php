60b697dfa8d485d81c1e62c74b11de1897be5639
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:39:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'យន្តការការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវផ្ដល់នូវ RADIUS បន្ថែមសម្រាប់ប្រព័ន្ធ។';
+$lang['radius_app_name'] = 'ម៉ាស៊ីនបម្រើ RADIUS';
+$lang['radius_client'] = 'ម៉ាស៊ីនភ្ញៀវ';
+$lang['radius_clients'] = 'អតិថិជន';
+$lang['radius_nickname'] = 'ឈ្មោះហៅក្រៅ';
+$lang['radius_nickname_invalid'] = 'ឈ្មោះហៅក្រៅមិនត្រឹមត្រូវ។';
+$lang['radius_radius_server'] = 'ម៉ាស៊ីនបម្រើ RADIUS';
