c03b76476a2b4bb64f8ef15bbdaa9ff39905f391
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:35:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS为系统提供额外的身份验证机制。';
+$lang['radius_app_name'] = 'RADIUS服务器';
+$lang['radius_client'] = '客户';
+$lang['radius_clients'] = '客户端';
+$lang['radius_nickname'] = '昵称';
+$lang['radius_nickname_invalid'] = '昵称是无效的。';
+$lang['radius_radius_server'] = 'RADIUS服务器';
