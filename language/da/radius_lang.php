700b48c6d5cb70afb0e87ece964a99768799f3fe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:35:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS giver yderligere mekanismer autentificering for systemet.';
+$lang['radius_app_name'] = 'RADIUS Server';
+$lang['radius_client'] = 'Klient';
+$lang['radius_clients'] = 'Kunder';
+$lang['radius_nickname'] = 'Brugernavn';
+$lang['radius_nickname_invalid'] = 'Alias ​​er ugyldig.';
+$lang['radius_radius_server'] = 'RADIUS Server';
