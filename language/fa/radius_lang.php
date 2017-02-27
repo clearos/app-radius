4d1f09e11cf8d177db56d07ef79995544cb5e235
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:42:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS فراهم می کند مکانیزم اعتبار اضافی برای سیستم.';
+$lang['radius_app_name'] = 'RADIUS سرور';
+$lang['radius_client'] = 'مشتری';
+$lang['radius_clients'] = 'مشتریان';
+$lang['radius_nickname'] = 'کنیه';
+$lang['radius_nickname_invalid'] = 'نام کاربری نامعتبر است.';
+$lang['radius_radius_server'] = 'RADIUS سرور';
