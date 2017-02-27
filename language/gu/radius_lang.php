d2ca2de222a00d6c6fdd8922287511e2b85956bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:37:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'રેડિયસ સિસ્ટમ માટે વધારાની સત્તાધિકરણ પદ્ધતિઓ પૂરી પાડે છે.';
+$lang['radius_app_name'] = 'રેડિયસ સર્વર';
+$lang['radius_client'] = 'ક્લાઈન્ટ';
+$lang['radius_clients'] = 'ક્લાઈન્ટો';
+$lang['radius_nickname'] = 'ઉપનામ';
+$lang['radius_nickname_invalid'] = 'ઉપનામ અમાન્ય છે.';
+$lang['radius_radius_server'] = 'રેડિયસ સર્વર';
