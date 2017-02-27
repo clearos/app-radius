a871ce713b827e3a0550b38c956ed7d74f8c757c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:44:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'ஆர அமைப்பு கூடுதல் அங்கீகார வழிமுறைகளை வழங்குகிறது.';
+$lang['radius_app_name'] = 'ஆர சர்வர்';
+$lang['radius_client'] = 'வாடிக்கையாளர்';
+$lang['radius_clients'] = 'வாடிக்கையாளர்';
+$lang['radius_nickname'] = 'புனைப்பெயர்';
+$lang['radius_nickname_invalid'] = 'புனைப்பெயர் தவறானது.';
+$lang['radius_radius_server'] = 'ஆர சர்வர்';
