2f3023d52f88520bd1ffb6f2bb7b0893f40ebf09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:37:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Reyon bay mekanism otantifikasyon anplis pou sistèm nan.';
+$lang['radius_app_name'] = 'reyon sèvè';
+$lang['radius_client'] = 'Kliyan';
+$lang['radius_clients'] = 'Kliyan';
+$lang['radius_nickname'] = 'Ti non jwèt';
+$lang['radius_nickname_invalid'] = 'Ti non jwèt se valab.';
+$lang['radius_radius_server'] = 'reyon sèvè';
