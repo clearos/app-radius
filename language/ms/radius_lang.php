abf9564223e4354eac49be1ce75ef07001227543
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:41:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS menyediakan mekanisme pengesahan tambahan untuk sistem.';
+$lang['radius_app_name'] = 'RADIUS Server';
+$lang['radius_client'] = 'Pelanggan';
+$lang['radius_clients'] = 'pelanggan';
+$lang['radius_nickname'] = 'Nickname';
+$lang['radius_nickname_invalid'] = 'Nama samaran yang tidak sah.';
+$lang['radius_radius_server'] = 'RADIUS Server';
