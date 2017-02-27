37a7a554990d0253271000bb458c58d1e85f03d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:34:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'ব্যাসার্ধ সিস্টেমের জন্য অতিরিক্ত প্রমাণীকরণ প্রক্রিয়া উপলব্ধ.';
+$lang['radius_app_name'] = 'ব্যাসার্ধ সার্ভার';
+$lang['radius_client'] = 'মক্কেল';
+$lang['radius_clients'] = 'ক্লায়েন্টদের মধ্যে';
+$lang['radius_nickname'] = 'ডাকনাম';
+$lang['radius_nickname_invalid'] = 'ডাকনাম অবৈধ.';
+$lang['radius_radius_server'] = 'ব্যাসার্ধ সার্ভার';
