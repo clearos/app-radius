6c0d6b2b724693c42256d4a0a25e47f26ea2fa8e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:41:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS biedt extra authenticatie mechanismen voor het systeem.';
+$lang['radius_app_name'] = 'RADIUS-server';
+$lang['radius_client'] = 'Cliënt';
+$lang['radius_clients'] = 'cliënten';
+$lang['radius_nickname'] = 'Bijnaam';
+$lang['radius_nickname_invalid'] = 'Bijnaam is ongeldig.';
+$lang['radius_radius_server'] = 'RADIUS-server';
