7e67c6ce121b359c4c0360005c926f92368ecda2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:44:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS ให้กลไกการตรวจสอบเพิ่มเติมสำหรับระบบ';
+$lang['radius_app_name'] = 'เซิร์ฟเวอร์ RADIUS';
+$lang['radius_client'] = 'ไคลเอนต์';
+$lang['radius_clients'] = 'ลูกค้า';
+$lang['radius_nickname'] = 'ชื่อเล่น';
+$lang['radius_nickname_invalid'] = 'ชื่อเล่นไม่ถูกต้อง';
+$lang['radius_radius_server'] = 'เซิร์ฟเวอร์ RADIUS';
