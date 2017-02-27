cbedb795a4a7500550dfd13dd6e2a245ea0edf16
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:36:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS provizas aldonan aŭtentokontrolo mekanismoj por la sistemo.';
+$lang['radius_app_name'] = 'RADIUS Servilo';
+$lang['radius_client'] = 'kliento';
+$lang['radius_clients'] = 'klientoj';
+$lang['radius_nickname'] = 'alnomo';
+$lang['radius_nickname_invalid'] = 'Alnomo estas nevalida.';
+$lang['radius_radius_server'] = 'RADIUS Servilo';
