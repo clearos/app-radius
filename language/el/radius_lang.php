3769e4d8554779da1ecacee83508d226f5a0d683
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:37:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS παρέχει πρόσθετη μηχανισμούς ελέγχου ταυτότητας για το σύστημα.';
+$lang['radius_app_name'] = 'Radius Server';
+$lang['radius_client'] = 'Πελάτης';
+$lang['radius_clients'] = 'Πελάτες';
+$lang['radius_nickname'] = 'Παρατσούκλι';
+$lang['radius_nickname_invalid'] = 'Ψευδώνυμο δεν είναι έγκυρο.';
+$lang['radius_radius_server'] = 'Radius Server';
