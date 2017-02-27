2f68e9800bb7bfe9f6dc8a83feb1478770ec3d24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:41:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'F&#39;radda jipprovdi mekkaniżmi ta &#39;awtentikazzjoni addizzjonali għas-sistema.';
+$lang['radius_app_name'] = 'F&#39;radda Server';
+$lang['radius_client'] = 'klijent';
+$lang['radius_clients'] = 'klijenti';
+$lang['radius_nickname'] = 'nickname';
+$lang['radius_nickname_invalid'] = 'Laqam huwa invalidu.';
+$lang['radius_radius_server'] = 'F&#39;radda Server';
