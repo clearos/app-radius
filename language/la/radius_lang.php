d5ed6c7785114be77355a0b4d01b2085bf79100d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:39:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS providet adiectis, authenticas declarandas ratio pro mechanisms.';
+$lang['radius_app_name'] = 'RADIUS Server';
+$lang['radius_client'] = 'client';
+$lang['radius_clients'] = 'Clients';
+$lang['radius_nickname'] = 'Tags';
+$lang['radius_nickname_invalid'] = 'Cognomentum est invalidum.';
+$lang['radius_radius_server'] = 'RADIUS Server';
