8f03d2babe1e80576f233a1bbc1cda895b6331aa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:38:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'რადიუსში დამატებით ავტორიზაციის მექანიზმები სისტემა.';
+$lang['radius_app_name'] = 'რადიუსში სერვერი';
+$lang['radius_client'] = 'კლიენტი';
+$lang['radius_clients'] = 'კლიენტები';
+$lang['radius_nickname'] = 'მეტსახელად';
+$lang['radius_nickname_invalid'] = 'მეტსახელად არასწორია.';
+$lang['radius_radius_server'] = 'რადიუსში სერვერი';
