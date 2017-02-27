6c0b83da181e8797bd95231826259dfcbb7e9f2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:45:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS cung cấp cơ chế xác thực bổ sung cho hệ thống.';
+$lang['radius_app_name'] = 'RADIUS server';
+$lang['radius_client'] = 'khách hàng';
+$lang['radius_clients'] = 'Khách hàng';
+$lang['radius_nickname'] = 'Tên nick';
+$lang['radius_nickname_invalid'] = 'Tên không hợp lệ.';
+$lang['radius_radius_server'] = 'RADIUS server';
