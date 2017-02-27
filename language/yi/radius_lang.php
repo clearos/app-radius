0ad5a25f710af9c4f8f2a6eb8e6c6989a57bf77f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:46:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'ראַדיוס גיט נאָך אָטענטאַקיישאַן מעקאַניזאַמז פֿאַר די סיסטעם.';
+$lang['radius_app_name'] = 'ראַדיוס סערווערס';
+$lang['radius_client'] = 'קליענט';
+$lang['radius_clients'] = 'קליענץ';
+$lang['radius_nickname'] = 'צונעמעניש';
+$lang['radius_nickname_invalid'] = 'ניקקנאַמע איז פאַרקריפּלט.';
+$lang['radius_radius_server'] = 'ראַדיוס סערווערס';
