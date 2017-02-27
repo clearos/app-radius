891f80749ac20820e4d51113194356e15367241e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:39:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Spindulys papildomų autentifikavimo mechanizmų sistemą.';
+$lang['radius_app_name'] = 'RADIUS serverį';
+$lang['radius_client'] = 'klientas';
+$lang['radius_clients'] = 'klientai';
+$lang['radius_nickname'] = 'pravardė';
+$lang['radius_nickname_invalid'] = 'Nickas yra neteisinga.';
+$lang['radius_radius_server'] = 'RADIUS serverį';
