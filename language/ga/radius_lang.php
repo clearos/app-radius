ab712229954b97baa15c31774f30e5d85e29b04e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:38:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'Soláthraíonn ga meicníochtaí fíordheimhnithe breise don chóras.';
+$lang['radius_app_name'] = 'RADIUS Freastalaí';
+$lang['radius_client'] = 'Cliant';
+$lang['radius_clients'] = 'cliaint';
+$lang['radius_nickname'] = 'leasainm';
+$lang['radius_nickname_invalid'] = 'Is leasainm neamhbhailí.';
+$lang['radius_radius_server'] = 'RADIUS Freastalaí';
