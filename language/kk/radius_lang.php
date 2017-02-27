29f39fdf4d4d87fa64c2b2f06af4cb63f99f61e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:38:59 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS жүйесі үшін қосымша аутентификация тетіктерін қамтамасыз етеді.';
+$lang['radius_app_name'] = 'RADIUS сервер';
+$lang['radius_client'] = 'клиент';
+$lang['radius_clients'] = 'Клиенттер';
+$lang['radius_nickname'] = 'Ник';
+$lang['radius_nickname_invalid'] = 'Ник жарамсыз болып табылады.';
+$lang['radius_radius_server'] = 'RADIUS сервер';
