77f6b9e2e11b81ec5abdf496d269139fc6e6febc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:46:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS yn darparu mecanweithiau dilysu ychwanegol ar gyfer y system.';
+$lang['radius_app_name'] = 'RADIUS Gweinydd';
+$lang['radius_client'] = 'cleient';
+$lang['radius_clients'] = 'cleientiaid';
+$lang['radius_nickname'] = 'llysenw';
+$lang['radius_nickname_invalid'] = 'Llysenw yn annilys.';
+$lang['radius_radius_server'] = 'RADIUS Gweinydd';
