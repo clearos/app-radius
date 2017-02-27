381252ecbb87472ce47c2fcdcb8184815bafe4db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-27 01:43:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/radius_lang.php
@@ -0,0 +1,9 @@
+<?php
+
+$lang['radius_app_description'] = 'RADIUS fana ka mekgwa ya tlatsetso netefatso bakeng tsamaiso.';
+$lang['radius_app_name'] = 'RADIUS Server';
+$lang['radius_client'] = 'sedirelwa';
+$lang['radius_clients'] = 'bareki';
+$lang['radius_nickname'] = 'boswaswi';
+$lang['radius_nickname_invalid'] = 'Boswaswi ha o sebetse.';
+$lang['radius_radius_server'] = 'RADIUS Server';
